8e8b3adf585f9ab470130f84deac7026631037a7
Rani Kumari <ranipurohit30@gmail.com>
2025-03-25 23:05:56 +0530

    increased input url box size and reduced margin-top

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -53,7 +53,7 @@ const Demo = () => {
   }
 
   return (
-    <section className= "mt-16 w-full max-w-xl">
+    <section className= "mt-8 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
