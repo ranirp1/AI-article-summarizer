8d9d8d36b9c73df33561bb284ad49bed17952e0e
Rani Kumari <ranipurohit30@gmail.com>
2025-03-27 18:16:42 +0530

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI_Article_Summarizer/'
+  base: '/AI_Article_Summarizer/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
