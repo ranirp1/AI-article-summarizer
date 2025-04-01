3295cd98c29a861a65cddd9c7c51d00570d03053
Rani Kumari <159254904+ranirp1@users.noreply.github.com>
2025-04-01 22:50:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-article-summarizer/',  
+  base: '/ai-article-summarizer/',  
   build: {
     rollupOptions: {
       output: {
